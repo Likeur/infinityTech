eda01f093126f7fe6f4c89d0c904a5b3ea24dda8
Likeur <jacksimba28@gmail.com>
2025-02-10 12:10:09 +0200

    fixing links

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
+  build: {
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'src/index.html'),
+            about: resolve(__dirname, 'src/pages/about.html'),
+            about: resolve(__dirname, 'src/pages/displayarticle.html'),
+            contact: resolve(__dirname, 'src/pages/contact.html'),
+        },
+    },
+},
 })
