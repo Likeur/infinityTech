05838900caa743c2a3160b951c8a32d151903f6c
Likeur <jacksimba28@gmail.com>
2025-02-14 11:50:14 +0200

    interaction on header nav

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const menuBtn = document.querySelector("#menu-btn");
 
 menuBtn.addEventListener("click", () => {
   mainHeader.dataset.state = mainHeader.dataset.state === "active" ? "closed" : "active";
-})
+}) 
 
 
 const html = document.documentElement;
@@ -15,6 +15,8 @@ const moonIcon = document.getElementById('moonIcon');
 
 // Vérifier si le mode sombre est déjà activé (par exemple, dans localStorage)
 const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+// Gerer le switch entre les icones
 if (isDarkMode) {
   html.classList.add('dark');
   sunIcon.style.display = 'block';
@@ -24,6 +26,7 @@ if (isDarkMode) {
   moonIcon.style.display = 'block';
 }
 
+// Ajouter un event au bouton qui vas switcher entre les 2 modes
 toggleButton.addEventListener('click', () => {
   html.classList.toggle('dark');
 
@@ -37,5 +40,24 @@ toggleButton.addEventListener('click', () => {
   } else {
     sunIcon.style.display = 'none';
     moonIcon.style.display = 'block';
+  }
+});
+
+
+
+// Sélectionner le header
+const header = document.querySelector('header');
+
+// Ajouter un gestionnaire d'événements pour détecter le scroll
+window.addEventListener('scroll', () => {
+  const scrollValue = window.scrollY; // Position verticale du scroll
+  const threshold = 50; // Valeur seuil
+
+  if (scrollValue > threshold) {
+    header.classList.add('bg-zinc-300/30', 'shadow-lg', 'lg:mt-6', 'lg:rounded-full', 'backdrop-blur-2xl', 'lg:border-b', 'lg:dark:border-white/40', 'lg:border-zinc-950/40', 'lg:w-[70vw]');
+    header.classList.remove('lg:bg-transparent','lg:w-[75vw]','lg:border-none', 'lg:backdrop-blur-none', 'bg-white/80', 'shadow-none');
+  } else {
+    header.classList.add('bg-white/80', 'lg:bg-transparent', 'lg:w-[75vw]', 'lg:border-none', 'lg:backdrop-blur-none', 'shadow-none');
+    header.classList.remove('bg-zinc-300/30', 'shadow-lg', 'lg:mt-6', 'lg:rounded-full', 'backdrop-blur-2xl', 'lg:border-b', 'lg:dark:border-white/40', 'lg:border-zinc-950/40', 'lg:w-[70vw]');
   }
 });
